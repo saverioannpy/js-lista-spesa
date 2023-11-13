e34e4ea17f9cb7c7f47e54d5daf1ca41c948b645
saverioannpy <saverio.annunziata@outlook.com>
2023-11-13 13:54:40 +0100

    fix: utilizzo variabile let

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const answer = prompt("Vuoi aggiungere qualcosa alla lista della spesa? (si/no)");
+let answer = prompt("Vuoi aggiungere qualcosa alla lista della spesa? (si/no)");
 console.log(answer);
 
 while( answer === "si"){
     const listElement = prompt("Cosa devi comprare?");
     console.log(listElement);
-    const answer = prompt("Vuoi aggiungere qualcosa alla lista della spesa? (si/no)");
+    answer = prompt("Vuoi aggiungere qualcosa alla lista della spesa? (si/no)");
     console.log(answer);
 }
