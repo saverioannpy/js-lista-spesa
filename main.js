2f5d6a0ca558fd93acb767f5c9cf56427f9ac296
saverioannpy <saverio.annunziata@outlook.com>
2023-11-13 15:22:27 +0100

    fix: ciclo do-while

--- main.js
+++ main.js
@@ -6,29 +6,34 @@ const elementUl = document.querySelector('ul.shopping-list'); //selezione l'elen
 console.log(elementUl);
 
 let answer ="si";
-let error = 0;
-answer = prompt("Vuoi aggiungere qualcosa alla lista della spesa? (si/no)"); //chiedo all'utente se deve inserire qualcosa all'interno della lista della spesa;
-console.log("Input utente: " + answer);
-answer = answer.toLocaleLowerCase(); //per evitare errori di input decido di rendere tutti i miei caratteri minuscoli;
-console.log("Testo convertito: " + answer);
+let error = 0; //variabile booleana che cambia valore in 1 nel caso in cui l'utente inseriza un input errato; 
 
 do{
-if(answer === "si" || answer === "no"){
-    while( answer === "si"){ //se l'utente risponde di si allora si entra all'interno del nostro ciclo;
-        const listElement = prompt("Cosa devi comprare?");//all'utente viene chiesto cosa vuole aggiungere alla propria lista;
-        console.log(listElement);
-        shoppingList.push(listElement); //inserisco l'elemento inserito nel mio array;
-        const li = document.createElement('li'); //creo un elemento li per il mio elenco non ordinato;
-        li.append(listElement); // "appendo" al mio elemento li l'input inserito dall'utente;
-        elementUl.append(li); // "appendo" all'elenco non ordinato il mio li;
-        answer = prompt("Vuoi aggiungere altro alla lista della spesa? (si/no)");//chiedo all'utente se deve aggiungere altro;
-        console.log(answer);
-    } //il ciclo si ripeterà fin quando l'utente continuerà a rispondere si;
-    console.log("Lista della spesa inserita dall'utente: " + shoppingList); 
-}else{
-    alert("Errore: Risposta non valida");
-    console.log("Errore: Risposta non valida");
-    error = 1;
-}
-}while(error=1);
-
+    answer = prompt("Vuoi aggiungere qualcosa alla lista della spesa? (si/no)"); //chiedo all'utente se deve inserire qualcosa all'interno della lista della spesa;
+    console.log("Input utente: " + answer);
+    answer = answer.toLocaleLowerCase(); //per evitare errori di input decido di rendere tutti i miei caratteri minuscoli;
+    console.log("Testo convertito: " + answer);
+    if(answer === "si"){
+        while(answer === "si"){ //se l'utente risponde di si allora si entra all'interno del nostro ciclo;
+            const listElement = prompt("Cosa devi comprare?");//all'utente viene chiesto cosa vuole aggiungere alla propria lista;
+            console.log(listElement);
+            shoppingList.push(listElement); //inserisco l'elemento inserito nel mio array;
+            const li = document.createElement('li'); //creo un elemento li per il mio elenco non ordinato;
+            li.append(listElement); // "appendo" al mio elemento li l'input inserito dall'utente;
+            elementUl.append(li); // "appendo" all'elenco non ordinato il mio li;
+            answer = prompt("Vuoi aggiungere altro alla lista della spesa? (si/no)");//chiedo all'utente se deve aggiungere altro;
+            console.log(answer); 
+            console.log("Testo convertito: " + answer);
+            answer = answer.toLocaleLowerCase(); //per evitare errori di input decido di rendere tutti i miei caratteri minuscoli;
+            console.log(answer); 
+        } //il ciclo si ripeterà fin quando l'utente continuerà a rispondere si;
+    }else if(answer === "no"){
+        alert("Allora hai il frigorifero già pieno, ottimo!");
+        console.log("Non c'è bisogno di fare la spesa! ");
+    }else{
+        alert("Errore: Risposta non valida");
+        console.log("Errore: Risposta non valida");
+        error = 1;
+    }
+}while(error === 1);
+console.log("Lista della spesa inserita dall'utente: " + shoppingList + "."); 
