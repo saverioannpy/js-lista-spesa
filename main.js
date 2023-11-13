9af04fe80da529e5478011c3ba360ebc1f1f088b
saverioannpy <saverio.annunziata@outlook.com>
2023-11-13 14:30:47 +0100

    feat: manipolazione del DOM

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 'use strict';
 
-let answer = prompt("Vuoi aggiungere qualcosa alla lista della spesa? (si/no)");
+const elementUl = document.querySelector('ul.shopping-list'); //selezione l'elenco non ordinato;
+console.log(elementUl);
+let answer = prompt("Vuoi aggiungere qualcosa alla lista della spesa? (si/no)"); //chiedo all'utente se deve inserire qualcosa all'interno della lista della spesa;
 console.log(answer);
 
-while( answer === "si"){
-    const listElement = prompt("Cosa devi comprare?");
+while( answer === "si"){ //se l'utente risponde di si allora si entra all'interno del nostro ciclo;
+    const listElement = prompt("Cosa devi comprare?");//all'utente viene chiesto cosa vuole aggiungere alla propria lista;
     console.log(listElement);
-    answer = prompt("Vuoi aggiungere qualcosa alla lista della spesa? (si/no)");
+    const li = document.createElement('li'); //creo un elemento li per il mio elenco non ordinato;
+    li.append(listElement); // "appendo" al mio elemento li l'input inserito dall'utente;
+    elementUl.append(li); // "appendo" all'elenco non ordinato il mio li;
+    answer = prompt("Vuoi aggiungere altro alla lista della spesa? (si/no)");//all'utente viene chiesto se deve aggiungere altro;
     console.log(answer);
-}
+} //il ciclo si ripeterà fin quando l'utente continuerà a rispondere si;
